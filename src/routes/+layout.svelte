08b6165824698b045c86b5e63edc4a1e133f94e8
remoflury <remoflury97+web@gmail.com>
2024-11-04 19:56:07 +0100

    setup supabase stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import '../css/main.css';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	let { children, data } = $props();
+	let supabase = $state(data.supabase);
+	let session = $state(data.session);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 {@render children()}
