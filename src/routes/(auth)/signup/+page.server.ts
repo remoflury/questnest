1af8438260f2cedf822f09662f1d1f0d127e92b7
remoflury <remoflury97+web@gmail.com>
2024-11-05 17:51:16 +0100

    set error for signing up

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -1,6 +1,6 @@
 import type { Actions, PageServerLoad } from "./$types.js";
 import { signupSchema } from "$lib/validation/schema.js";
-import { message, superValidate } from "sveltekit-superforms";
+import { message, setError, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { setFlash } from "sveltekit-flash-message/server";
 import { redirect } from "sveltekit-flash-message/server";
@@ -23,9 +23,15 @@ export const actions: Actions = {
       email: form.data.email,
       password: form.data.password
     })
-
+    
     if (error) {
-      console.error(error)
+      console.error("eeeerr", error)
+      if (error.code == 'user_already_exists') {
+        setFlash({ type: "error", message: "User with this email already exists."}, cookies)
+        setError(form, 'email', "User with this email already exists.")
+        return message(form, "User with this email already exists.", { status: 400 })
+      }
+      
       setFlash({ type: 'error', message: 'Something went wrong. Try again later.' }, cookies);
       return message(form, "Something went wrong. Try again later.", { status: 400 })
     }
