1c893fb11c75bcb826c09d8878105b4fa9df9a97
remoflury <remoflury97+web@gmail.com>
2024-11-06 14:43:35 +0100

    page structure

--- src/routes/(app)/quests/+page.svelte
+++ src/routes/(app)/quests/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Queests</h1>
+<script lang="ts">
+	import FadeInWrapper from '$lib/components/general/FadeInWrapper.svelte';
+
+	// let { data } = $props();
+</script>
+
+<FadeInWrapper class="section-spacing container" tag="section">
+	<h1>Quests</h1>
+</FadeInWrapper>
