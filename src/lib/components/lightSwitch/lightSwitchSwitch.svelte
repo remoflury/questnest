e4c064387b58c95e471e65020dddfa66fdee4b6b
remoflury <remoflury97+web@gmail.com>
2024-11-19 15:04:33 +0100

    fix bug on switching sites

--- src/lib/components/lightSwitch/lightSwitchSwitch.svelte
+++ src/lib/components/lightSwitch/lightSwitchSwitch.svelte
@@ -3,8 +3,7 @@
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { Switch } from '$lib/components/ui/switch/index.js';
 	import { cn } from '$lib/utils/utils';
-	import { mode } from 'mode-watcher';
-	import { setMode } from 'mode-watcher';
+	import { mode, toggleMode } from 'mode-watcher';
 
 	type Props = HTMLAttributes<HTMLDivElement> & {
 		id: string;
@@ -12,15 +11,18 @@
 
 	let { class: className, id }: Props = $props();
 	let checked = $state($mode == 'light');
-
-	$effect(() => {
-		if (checked) setMode('dark');
-		if (!checked) setMode('light');
-	});
 </script>
 
 <div class={cn('flex items-center space-x-2', className)}>
 	<Label for={id}>Light Mode</Label>
-	<Switch {id} bind:checked />
+	<Switch
+		{id}
+		bind:checked
+		onclick={toggleMode}
+		onkeydown={(e) => {
+			if (e.code != 'Enter') return;
+			toggleMode();
+		}}
+	/>
 	<Label for={id}>Dark Mode</Label>
 </div>
