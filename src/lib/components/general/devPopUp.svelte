ef1f2a67e6619a3c25303a5b1e7e036229a77b54
remoflury <remoflury97+web@gmail.com>
2024-12-11 11:40:51 +0100

    fix overflow bug

--- src/lib/components/general/devPopUp.svelte
+++ src/lib/components/general/devPopUp.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
 	type Props = {
 		open: boolean;
 	};
 
 	let { open = $bindable() }: Props = $props();
-
-	$effect(() => {
-		if (browser) {
-			document.body.classList.toggle('overflow-hidden', open);
-		}
-	});
 </script>
 
 <AlertDialog.Root bind:open>
-	<!-- <AlertDialog.Trigger>Open</AlertDialog.Trigger> -->
 	<AlertDialog.Content>
 		<AlertDialog.Header>
 			<AlertDialog.Title>Important Information</AlertDialog.Title>
